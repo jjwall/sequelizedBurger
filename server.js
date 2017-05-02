d048623e8821291f20ed9065f8919d730d621ef3
Jacob Wallace <jacob.wallace@utexas.edu>
2017-05-02 17:53:50 -0500

    sequelize working, need to get handlebars running

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 //var path = require("path");
 
-var port = 3000;
-
 var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+var db = require("./models");
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
@@ -23,8 +25,15 @@ app.engine("handlebars", exphbs({
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
+//var routes = require("./controllers/burgers_controller.js");
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
-app.use("/", routes);
+//app.use("/", routes);
 
-app.listen(port);
+// Syncing our sequelize models and then starting our express app
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
